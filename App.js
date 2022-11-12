5a464e7460db575d695b8e3fd251726855032aec
Nasyx Rakeeb <nasyxrakeeb2@gmail.com>
2022-11-12 20:27:47 +0530

    initial setup with nav

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Home from "./screens/Home.js"
+import Matches from "./screens/Matches.js"
+import News from "./screens/News.js"
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
-export default function App() {
+const Tab = createBottomTabNavigator()
+const navTheme = DefaultTheme
+navTheme.colors.background = "#fff"
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer theme={MyTheme}>
       <StatusBar style="auto" />
-    </View>
+      <Tab.Navigator screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName
+            if (route.name === 'Home') {
+              iconName = focused ? 'ios-information-circle' : 'ios-information-circle-outline'
+            } else if (route.name === 'Matches') {
+              iconName = focused ? 'ios-list' : 'ios-list-outline'
+            } else if (route.name === "News") {
+              iconName = focused ? "ios-information-circle" : "ios-list-outline"
+            }
+            return <Ionicons name={iconName} size={size} color={color} />
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Matches" component={Matches} />
+        <Tab.Screen name="News" component={News} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
---
+++ screens/Matches.js
@@ -0,0 +1,17 @@
+import {View, Text, StyleSheet} from "react-native"
+
+const = () => {
+  return (
+    <View style={styles.container}>
+    
+    </View>
+    )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
+
+expo default 
